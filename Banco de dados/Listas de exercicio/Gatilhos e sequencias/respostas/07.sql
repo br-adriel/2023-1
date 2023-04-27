13be1cadeaf6531ec50ebf50ae74fbcfa5f53209
Adriel <adriel.fsantos@outlook.com>
2023-04-26 22:06:24 -0300

    Corrige resposta da questao 7 da lista de sequencia

--- Banco de dados/Listas de exercicio/Gatilhos e sequencias/respostas/07.sql
+++ Banco de dados/Listas de exercicio/Gatilhos e sequencias/respostas/07.sql
@@ -1,4 +1,6 @@
 CREATE TRIGGER add_passageiro AFTER INSERT ON cliente_voo FOR EACH ROW
 UPDATE voo
 SET
-  num_passageiros = num_passageiros + 1;
+  num_passageiros = num_passageiros + 1
+WHERE
+  codigo = NEW.voo;
