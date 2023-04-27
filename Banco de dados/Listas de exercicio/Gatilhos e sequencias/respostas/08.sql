427d4539c4e8ddf582663b145e155e63260e856d
Adriel <adriel.fsantos@outlook.com>
2023-04-26 23:02:02 -0300

    Resolve questao 8 da lista de sequencia

---
+++ Banco de dados/Listas de exercicio/Gatilhos e sequencias/respostas/08.sql
@@ -0,0 +1,10 @@
+CREATE TRIGGER queda_aviao_clientes BEFORE DELETE ON voo FOR EACH ROW BEGIN
+DELETE FROM cliente
+JOIN cliente_voo ON cliente.codigo = cliente_voo.cliente
+WHERE
+  cliente_voo.voo = OLD.codigo;
+
+CREATE TRIGGER queda_aviao_clientes BEFORE DELETE ON voo FOR EACH ROW
+DELETE FROM piloto
+WHERE
+  codigo = OLD.piloto;
