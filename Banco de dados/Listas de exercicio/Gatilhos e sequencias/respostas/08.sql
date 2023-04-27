0cdfbb2c45a879b6d6e5c561857ca92415037000
Adriel <adriel.fsantos@outlook.com>
2023-04-26 23:06:57 -0300

    Corrige nome do trigger da questao 8 da lista de sequencia

--- Banco de dados/Listas de exercicio/Gatilhos e sequencias/respostas/08.sql
+++ Banco de dados/Listas de exercicio/Gatilhos e sequencias/respostas/08.sql
@@ -4,7 +4,7 @@ JOIN cliente_voo ON cliente.codigo = cliente_voo.cliente
 WHERE
   cliente_voo.voo = OLD.codigo;
 
-CREATE TRIGGER queda_aviao_clientes BEFORE DELETE ON voo FOR EACH ROW
+CREATE TRIGGER queda_aviao_piloto BEFORE DELETE ON voo FOR EACH ROW
 DELETE FROM piloto
 WHERE
   codigo = OLD.piloto;
