7168362ba97421357652085e2ef9caa36d4f9303
Adriel <adriel.fsantos@outlook.com>
2023-05-22 17:35:48 -0300

    Estiliza textos do app

--- POO2/flutter/receita1/lib/main.dart
+++ POO2/flutter/receita1/lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:styled_text/styled_text.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            textTheme:
+                GoogleFonts.outfitTextTheme(Theme.of(context).textTheme)),
+        home: Scaffold(
+          appBar: AppBar(title: const Text("Meu app")),
+          body: Center(
+              child: Column(children: [
+            const Text("Apenas começando..."),
+            StyledText(
+              text: "<i>Olá</i> <b>mundo</b>",
+              tags: {
+                'b': StyledTextTag(
+                    style: const TextStyle(fontWeight: FontWeight.bold)),
+                'i': StyledTextTag(
+                    style: const TextStyle(
+                        fontStyle: FontStyle.italic, fontSize: 20)),
+              },
+            ),
+            const Text("Terminando...")
+          ])),
+          bottomNavigationBar: const Text("Botão 1"),
+        ));
+  }
+}
 
 void main() {
-  MaterialApp app = MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Meu app")),
-        body: const Center(
-            child: Column(children: [
-          Text("Apenas começando..."),
-          Text("No meio..."),
-          Text("Terminando...")
-        ])),
-        bottomNavigationBar: const Text("Botão 1"),
-      ));
-
+  MyApp app = MyApp();
   runApp(app);
 }
