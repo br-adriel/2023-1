e51e8e34bbed84a5e4b2edd377155ee3da0286b3
Adriel <adriel.fsantos@outlook.com>
2023-05-29 15:16:23 -0300

    Implementa uso do MobX sem gerador de código

--- POO2/flutter/mobx_medium/lib/main.dart
+++ POO2/flutter/mobx_medium/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'controller/controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,18 +57,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final controller = Controller();
 
   @override
   Widget build(BuildContext context) {
@@ -108,15 +99,17 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Observer(builder: (_) {
+              return Text(
+                '${controller.numeroDeClicks.value}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              );
+            }),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: controller.addClick,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
